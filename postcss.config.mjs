9760683ad025a4a3410922580b49b2569af5854b
SiyaPockle <166788343+SiyaPockle@users.noreply.github.com>
2025-05-31 20:17:01 +0530

    update postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
-export default {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> 6123fb808b6784233ae6554d0ffd77e5e1f707b1
 };
